730dd00b1168ec409a20cbd474a780e8f2a9f0b2
mike <mikecaines@gmail.com>
2015-11-29 11:08:07 -0400

    Environment: Create globals for Lightship -> Solarfield.Lightship, for convenience

--- src/Lightship/Bin/lightship/stub/www/__/App/Environment.js
+++ src/Lightship/Bin/lightship/stub/www/__/App/Environment.js
@@ -1,3 +1,11 @@
+//import some Solarfield stuff as globals for convenience
+if (!window.Ok) window.Ok = Solarfield.Ok;
+if (!window.Batten) window.Batten = Solarfield.Batten;
+if (!window.Lightship) window.Lightship = Solarfield.Lightship;
+
+
+
+
 /**
  * @namespace App
  */
